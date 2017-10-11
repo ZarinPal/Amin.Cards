bf2864b95a5aeed066a6bf7ffdd1569d1148c553
Amin <ak_@yahoo.com>
2017-10-11 14:26:52 +0330

    Initial commit

---
+++ compiler.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+npm install -g gulp
+npm install
+git config --global user.email "ZarinPal@Travis"
+git config --global user.name "ZarinPal@Travis"
+git config --global push.default simple
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 .deploy_key # this key should have push access
+ssh-add .deploy_key
+git clone -b gh-pages git@github.com:ZarinPal/bingo.ir.git public
+gulp --production
+cd public
+git add --all .
+git commit -m "new release"
+git push origin HEAD:gh-pages
