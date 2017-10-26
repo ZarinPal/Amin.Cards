0e932d1e5a971e67dc68d60474fd66aa6debb021
Amin <ak_1596@yahoo.com>
2017-10-26 14:24:56 +0330

    Fix header

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const elixir = require('laravel-elixir');
 require('laravel-elixir-pug');
 
-
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,46 @@
 /**
  * Created by Amin Keshavarz on 10/13/2017.
  */
+/**
+ * Header background movement by mouse over.
+ */
+// let movementStrength = 40;
+// let height = movementStrength / window.innerHeight;
+// let width = movementStrength / window.innerWidth;
+// let header = document.querySelector('header');
+// document.addEventListener("mouseover", function(e){
+//     if(e.pageY <= header.clientHeight){
+//         let pageX = e.pageX - (window.innerWidth / 2);
+//         let pageY = e.pageY - (window.innerHeight / 2);
+//         let signX = pageX && pageX / Math.abs(pageX);
+//         let signY = pageY && pageY / Math.abs(pageY);
+//         let newvalueX1 = 300 + ((pageX) * -1 - movementStrength);
+//         let newvalueY1 = -25 + ((pageY) * -1 + movementStrength);
+//         let newvalueX2 = 30 + ((pageX) * -1 - movementStrength);
+//         let newvalueY2 = 25 + ((pageY) * -1 + movementStrength);
+//         header.style.backgroundPosition = ;
+//     }
+// });
+jQuery(document).ready(function () {
+    let movementStrength = 100;
+    let height = movementStrength / jQuery(window).height();
+    let width = movementStrength / jQuery(window).width();
+    jQuery("header").mousemove(function (e) {
+        console.log(e.pageX);
+        let pageX = e.pageX - (jQuery(window).width() / 2);
+        let pageY = e.pageY - (jQuery(window).height() / 2);
+        let newvalueX1 = width * pageX * -1 + 300;
+        let newvalueY1 = height * pageY * -1 - 25;
+        let newvalueX2 = width * pageX * +1 + 30;
+        let newvalueY2 = height * pageY * +1 + 25;
+        jQuery('header').css("background-position", newvalueX1 + "px " + newvalueY1 + "px, " + newvalueX2 + "px " + newvalueY2 + "px, center");
+    });
+});
+/* ./End header movement action. */
 
+/**
+ * Card properties active and un active action.
+ */
 let el = document.querySelectorAll('#properties li');
 el.forEach(function (value, key) {
     value.onclick = function () {
@@ -12,3 +51,4 @@ el.forEach(function (value, key) {
         value.classList.add('active');
     };
 });
+/* End of card activation */
